c26a0047449ddcf666d3f4fab420ff2ca21f3227
Shahil <shahil18056@gmail.com>
2023-09-09 10:06:48 +0530

    hello

--- firebase.js
+++ firebase.js
@@ -1,14 +1,4 @@
-/**
- * stable version was
- * npm install --save firebase-admin@10.3 
- * @param cert, initializeApp
- * @exports db
- * git cmd
- * git rebase --onto
- * git cherry-pick
- * sudo lsof -i :port 
- * sudo kill 
- */
+
 const admin = require("firebase-admin");
 const credentials = require("./key.json");
 admin.initializeApp({
